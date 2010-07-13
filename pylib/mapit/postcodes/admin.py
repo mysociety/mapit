4640e897293bf03d5b48355bffaa38b6622ce388
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-07-13 17:13:37 +0100

    Try admin inline.

--- pylib/mapit/postcodes/admin.py
+++ pylib/mapit/postcodes/admin.py
@@ -1,8 +1,15 @@
 from django.contrib.gis import admin
 from models import Postcode
 
+class AreaInline(admin.TabularInline):
+    model = Postcode.areas.through
+
 class PostcodeAdmin(admin.OSMGeoAdmin):
     search_fields = ['postcode']
+    inlines = [
+        AreaInline,
+    ]
+    exclude = ('areas',)
 
 admin.site.register(Postcode, PostcodeAdmin)
 
