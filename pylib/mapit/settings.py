80890dd04f10b404087aa5994afc0da9addc842c
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-07-13 16:22:08 +0100

    Template dir.

--- pylib/mapit/settings.py
+++ pylib/mapit/settings.py
@@ -84,6 +84,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    package_dir + '/templates',
 )
 
 INSTALLED_APPS = (
