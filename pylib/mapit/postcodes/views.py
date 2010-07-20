f329c8481acd51c3102dabcfef64a7825784129b
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-07-20 12:56:35 +0100

    Start of views for postcode lookups.

--- pylib/mapit/postcodes/views.py
+++ pylib/mapit/postcodes/views.py
@@ -1,27 +1,58 @@
 import re
 import itertools
 from mapit.postcodes.models import Postcode
+from mapit.postcodes.utils import is_valid_postcode, is_valid_partial_postcode
 from mapit.areas.models import Area, Generation
 from django.shortcuts import get_object_or_404
 from django.utils import simplejson
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 
-def postcode(request, postcode, format='html'):
+# Stupid fixed IDs from old MaPit
+WMP_AREA_ID = 900000
+EUP_AREA_ID = 900001
+LAE_AREA_ID = 900002
+SPA_AREA_ID = 900003
+WAS_AREA_ID = 900004
+NIA_AREA_ID = 900005
+LAS_AREA_ID = 900006
+HOL_AREA_ID = 900007
+HOC_AREA_ID = 900008
+enclosing_areas = {
+    'LAC': [ LAE_AREA_ID, LAS_AREA_ID ],
+    'SPC': [ SPA_AREA_ID ],
+    'WAC': [ WAS_AREA_ID ],
+    'NIE': [ NIA_AREA_ID ],
+    'WMC': [ WMP_AREA_ID ],
+    'EUR': [ EUP_AREA_ID ],
+}
+
+class Http400(Exception):
+    pass
+
+def _postcode(request, postcode):
     postcode = re.sub('\s+', '', postcode.upper())
+    if not is_valid_postcode(postcode):
+        raise Http400
     postcode = get_object_or_404(Postcode, postcode=postcode)
-    current_generation = Generation.objects.current()
-    areas = []
-    for area in itertools.chain(
-        Area.objects.filter(
-            polygons__polygon__contains=postcode.location,
-            generation_low__lte=current_generation,
-            generation_high__gte=current_generation
-        ),
-        postcode.areas.filter(
-            generation_low__lte=current_generation,
-            generation_high__gte=current_generation
-        )
-    ):
+    generation = request.REQUEST.get('generation', Generation.objects.current())
+    areas = Area.objects.by_postcode(postcode, generation)
+
+    # Add fictional enclosing areas. 
+    extra = []
+    for area in areas:
+        if area.type in enclosing_areas.keys():
+            extra.extend(enclosing_areas[area.type])
+    areas = itertools.chain(araes, Area.objects.filter(id__in=extra))
+ 
+    return areas
+    
+def postcode(request, postcode, format='html'):
+    try:
+        lookup = _postcode(request, postcode)
+    except Http400:
+        return HttpResponseBadRequest("Postcode '%s' is not valid." % postcode)
+    areas = {}
+    for area in lookup:
         areas.append({
             'id': area.id,
             'name': area.name,
@@ -32,8 +63,63 @@ def postcode(request, postcode, format='html'):
             'generation_high': area.generation_high.id,
             'codes': area.all_codes,
         })
-
     response = HttpResponse(content_type='application/javascript; charset=utf-8')
     simplejson.dump(areas, response, ensure_ascii=False)
     return response
     
+# OLD VIEWS
+
+def get_voting_areas(request):
+    postcode = request.REQUEST['postcode']
+    try:
+        lookup = _postcode(request, postcode)
+    except Http400:
+        return HttpResponseBadRequest("Postcode '%s' is not valid." % postcode)
+    areas = {}
+    for area in lookup:
+        areas[area.type] = area.id
+    response = HttpResponse(content_type='application/javascript; charset=utf-8')
+    simplejson.dump(areas, response, ensure_ascii=False)
+    return response
+
+def get_example_postcode(request, area_id):
+    area = get_object_or_404(Area, id=area_id)
+    pc = Postcode.objects.filter(areas=area)[0]
+    if not pc:
+        pc = Postcode.objects.filter(location__contained=area.polygons.all().collect()).order_by('?')[0]
+    return HttpResponse(pc)
+
+def get_location(request):
+    postcode = re.sub('\s+', '', request.REQUEST['postcode']).upper()
+    partial = request.REQUEST['partial']
+    irish = (postcode[0:2] == 'BT')
+
+    result = {}
+    if re.match('ZZ9', postcode): return result
+    if partial:
+        if is_valid_postcode(postcode):
+            postcode = re.sub('\d[A-Z]{2}$', '', postcode)
+        if not is_valid_partial_postcode(postcode):
+            return HttpResponseBadRequest("Partial postcode '%s' is not valid." % postcode)
+        loc = Postcode.objects.filter(postcode__startswith=postcode).collect().centroid()
+    else:
+        if not is_valid_postcode(postcode):
+            return HttpResponseBadRequest("Postcode '%s' is not valid." % postcode)
+        try:
+            postcode = Postcode.objects.get(postcode=postcode)
+        except Postcode.DoesNotExist:
+            raise Http404
+        loc = postcode.location
+
+    result['wgs84_lon'] = loc[0]
+    result['wgs84_lat'] = loc[1]
+    if irish:
+        loc.transform(29902)
+        result['coordsyst'] = 'I'
+    else:
+        loc.transform(27700)
+        result['coordsyst'] = 'G'
+    result['easting'] = loc[0]
+    result['northing'] = loc[1]
+
+    return result
--- pylib/mapit/urls.py
+++ pylib/mapit/urls.py
@@ -7,4 +7,17 @@ urlpatterns = patterns('',
     (r'^postcode/(?P<postcode>[a-z0-9 ]+)(?:\.(?P<format>json))?$(?i)', 'mapit.postcodes.views.postcode'),
     (r'^area/(?P<area_id>[0-9 ]+)(?:\.(?P<format>json))?$(?i)', 'mapit.areas.views.area'),
     (r'^admin/', include(admin.site.urls)),
+
+    # Old style calls
+    (r'^get_voting_areas$', 'mapit.postcodes.views.get_voting_areas'),
+#    (r'^get_voting_area_info$', 'mapit.areas.views.get_voting_area_info'),
+#    (r'^get_voting_areas_info$', 'mapit.areas.views.get_voting_areas_info'),
+#    (r'^get_voting_area_by_name$', 'mapit.areas.views.get_voting_area_by_name'),
+#    (r'^get_areas_by_type$', 'mapit.areas.views.get_areas_by_type'),
+#    (r'^get_voting_area_geometry$', 'mapit.areas.views.get_voting_area_geometry'),
+#    (r'^get_voting_areas_geometry$', 'mapit.areas.views.get_voting_areas_geometry'),
+#    (r'^get_voting_areas_by_location$', 'mapit.areas.views.get_voting_areas_by_location'),
+    (r'^get_example_postcode$', 'mapit.postcodes.views.get_example_postcode'),
+#    (r'^get_voting_area_children$', 'mapit.areas.views.get_voting_area_children'),
+    (r'^get_location$', 'mapit.postcodes.views.get_location'),
 )
