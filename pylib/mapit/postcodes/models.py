7fe24724c598050d938a2a61a875c7660ed74aa7
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-07-20 12:24:25 +0100

    Prevent circular.

--- pylib/mapit/postcodes/models.py
+++ pylib/mapit/postcodes/models.py
@@ -1,13 +1,12 @@
 import re
 from mapit.managers import GeoManager
 from django.contrib.gis.db import models
-from mapit.areas.models import Area
 
 class Postcode(models.Model):
     postcode = models.CharField(max_length=7, db_index=True, unique=True)
     location = models.PointField()
     # Will hopefully use PostGIS point-in-polygon tests, but if we don't have the polygons...
-    areas = models.ManyToManyField(Area, related_name='postcodes')
+    areas = models.ManyToManyField('areas.Area', related_name='postcodes')
 
     objects = GeoManager()
 
