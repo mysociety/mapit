f775c4ddff8a91c366a173ba1808031a24d2e20f
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-07-21 17:27:00 +0100

    DB debugging.

--- pylib/mapit/shortcuts.py
+++ pylib/mapit/shortcuts.py
@@ -1,5 +1,7 @@
 from django.utils import simplejson
 from django.http import HttpResponse
+from django.db import connection
+from django.conf import settings
 
 class GEOS_JSONEncoder(simplejson.JSONEncoder):
     def default(self, o):
@@ -11,6 +13,10 @@ class GEOS_JSONEncoder(simplejson.JSONEncoder):
 
 def output_json(out):
     response = HttpResponse(content_type='application/javascript; charset=utf-8')
-    simplejson.dump(out, response, ensure_ascii=False, cls=GEOS_JSONEncoder, indent=4)
+    if settings.DEBUG:
+        simplejson.dump(out, response, ensure_ascii=False, cls=GEOS_JSONEncoder, indent=4)
+        simplejson.dump(connection.queries, response, ensure_ascii=False, cls=GEOS_JSONEncoder, indent=4)
+    else:
+        simplejson.dump(out, response, ensure_ascii=False, cls=GEOS_JSONEncoder)
     return response
 
