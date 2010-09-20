85a8b7c163059043f494e06bfc995250365c1899
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-20 22:41:17 +0100

    Crown dependency postcode import script.

---
+++ pylib/mapit/postcodes/management/commands/import_nspd_crown_dependencies.py
@@ -0,0 +1,35 @@
+# This script is used to import Crown Dependency postcode information from the
+# National Statistics Postcode Database.
+# http://www.ons.gov.uk/about-statistics/geography/products/geog-products-postcode/nspd/
+
+import csv
+from django.core.management.base import LabelCommand
+from mapit.postcodes.models import Postcode
+
+class Command(LabelCommand):
+    help = 'Imports Crown Dependency postcodes from the NSPD'
+    args = '<NSPD CSV file>'
+
+    def handle_label(self, file, **options):
+        count = { 'total': 0, 'exists': 0, 'created': 0 }
+        for row in csv.reader(open(file)):
+            if row[4]: continue # Terminated postcode
+            if row[11] == '9': continue # PO Box etc.
+
+            postcode = row[0].strip().replace(' ', '')
+            if postcode[0:2] not in ('GY', 'JE', 'IM'): continue # Only importing Crown dependencies from NSPD
+
+            # Create/update the postcode
+            try:
+                pc = Postcode.objects.get(postcode=postcode)
+                count['exists'] += 1
+            except Postcode.DoesNotExist:
+                pc = Postcode.objects.create(postcode=postcode)
+                count['created'] += 1
+
+            count['total'] += 1
+            if count['total'] % 1000 == 0:
+                print "Imported %d (%d new, %d already there)" % (
+                    count['total'], count['created'], count['exists']
+                )
+
