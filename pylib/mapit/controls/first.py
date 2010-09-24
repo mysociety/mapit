24c4d5de37e9a65c5ca50cd90040f19e74ce08e0
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-24 18:48:56 +0100

    Tweaks to README, hopefully clearer for normal installation, thanks Anna.

---
+++ pylib/mapit/controls/first.py
@@ -0,0 +1,13 @@
+# A control file for importing Boundary-Line.
+# Not all areas have ONS codes, so we have to have something
+# manual to e.g. tell us if some WMC have changed.
+#
+# Things without ONS codes: CED EUR GLA LAC SPC SPE WAC WAE WMC
+# 
+# For Oct 2009, it doesn't matter what this returns, as it's
+# the first Open version and the database will/should be empty.
+
+def check(name, type, country, geometry):
+    """Should return True if this area is NEW, False if we should match"""
+    return False
+
