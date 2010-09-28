bbe3c039b16ab557b50c94a62b0dfd3616764b4e
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-28 17:22:36 +0100

    Optional callback for postcode only at present.

--- pylib/mapit/shortcuts.py
+++ pylib/mapit/shortcuts.py
@@ -1,3 +1,4 @@
+import re
 from django.utils import simplejson
 from django import http
 from django.db import connection
@@ -17,7 +18,7 @@ class GEOS_JSONEncoder(DjangoJSONEncoder):
 def output_html(areas):
     return render_to_response('data.html', { 'areas': areas })
 
-def output_json(out, code=200):
+def output_json(out, code=200, request=None):
     types = {
         400: http.HttpResponseBadRequest,
         404: http.HttpResponseNotFound,
@@ -33,7 +34,10 @@ def output_json(out, code=200):
         if isinstance(out, dict):
             out['debug_db_queries'] = connection.queries
         indent = 4
+    if request and request.GET.get('callback') and re.match('[a-zA-Z0-9_]+$', request.GET.get('callback')):
+        response.write(request.GET.get('callback') + '(')
     simplejson.dump(out, response, ensure_ascii=False, cls=GEOS_JSONEncoder, indent=indent)
+    response.write(')')
     return response
 
 def get_object_or_404(klass, *args, **kwargs):
