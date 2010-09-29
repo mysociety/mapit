dac3cedaee583db4cc3c05a9cb2c4f15a707123e
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-29 14:06:07 +0100

    Set up JSONP requests to always return 200.

--- pylib/mapit/middleware.py
+++ pylib/mapit/middleware.py
@@ -4,5 +4,6 @@ class JSONPMiddleware(object):
     def process_response(self, request, response):
         if request.GET.get('callback') and re.match('[a-zA-Z0-9_]+$', request.GET.get('callback')):
             response.content = request.GET.get('callback') + '(' + response.content + ')'
+            response.status_code = 200 # Must return OK for JSONP to be processed
         return response
 
