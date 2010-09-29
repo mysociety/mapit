2b664e34ce66621d911a71749da0f14d436e0412
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-09-29 13:41:21 +0100

    Missing import.

--- pylib/mapit/middleware.py
+++ pylib/mapit/middleware.py
@@ -1,3 +1,5 @@
+import re
+
 class JSONPMiddleware(object):
     def process_response(self, request, response):
         if request.GET.get('callback') and re.match('[a-zA-Z0-9_]+$', request.GET.get('callback')):
