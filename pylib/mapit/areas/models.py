e78d72f97b3a94f548658457546220c517014b48
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-10-09 02:08:35 +0100

    Specify the join.

--- pylib/mapit/areas/models.py
+++ pylib/mapit/areas/models.py
@@ -54,7 +54,7 @@ class AreaManager(models.GeoManager):
     def intersect(self, query_type, area):
         return Area.objects.exclude(id=area.id).extra(
             tables = [ 'areas_geometry' ],
-            where = [ 'ST_%s(polygon, (select polygon from areas_geometry where area_id=%%s))' % query_type ],
+            where = [ 'areas_geometry.area_id = areas_area.id', 'ST_%s(areas_geometry.polygon, (select polygon from areas_geometry where area_id=%%s))' % query_type ],
             params = [area.id]
         )
 
