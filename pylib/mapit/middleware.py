685d9147092a2e8fe67d75275204522e62ec6d7c
Matthew Somerville <matthew@bitter.ukcod.org.uk>
2010-10-11 16:04:47 +0100

    Encode callback for content.

--- pylib/mapit/middleware.py
+++ pylib/mapit/middleware.py
@@ -3,7 +3,7 @@ import re
 class JSONPMiddleware(object):
     def process_response(self, request, response):
         if request.GET.get('callback') and re.match('[a-zA-Z0-9_]+$', request.GET.get('callback')):
-            response.content = request.GET.get('callback') + '(' + response.content + ')'
+            response.content = request.GET.get('callback').encode('utf-8') + '(' + response.content + ')'
             response.status_code = 200 # Must return OK for JSONP to be processed
         return response
 
