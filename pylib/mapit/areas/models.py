cfd3e5a64c877468077b7d2ac6a5be1a7184e337
Matthew Somerville <matthew@arrow.ukcod.org.uk>
2011-08-08 16:12:08 +0100

    Now we're on 8.4, try putting b1df82a8eff27c336d864dcfe5739fa3a0643198 back in, explicitly force a && bbox index.

--- pylib/mapit/areas/models.py
+++ pylib/mapit/areas/models.py
@@ -103,7 +103,11 @@ class AreaManager(models.GeoManager):
     def intersect(self, query_type, area):
         return Area.objects.exclude(id=area.id).extra(
             tables = [ 'areas_geometry' ],
-            where = [ 'areas_geometry.area_id = areas_area.id', 'ST_%s(areas_geometry.polygon, (select polygon from areas_geometry where area_id=%%s))' % query_type ],
+            where = [
+                'areas_geometry.area_id = areas_area.id',
+                'ST_%s(areas_geometry.polygon, (select polygon from areas_geometry where area_id=%%s))' % query_type
+                'areas_geometry.polygon && (select polygon from areas_geometry where area_id=%s)'
+            ],
             params = [ area.id ]
         )
 
