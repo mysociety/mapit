68e70b1305d0f5ca5e03038f722ca9cf023445ac
Mark Longair <mhl@pobox.com>
2012-06-07 07:08:56 +0100

    An improved version of get-boundaries-from-planet.py

--- bin/get-boundaries-from-planet.py
+++ bin/get-boundaries-from-planet.py
@@ -11,6 +11,8 @@ import urllib, urllib2
 from boundaries import *
 from generate_kml import *
 
+from lxml import etree
+
 if len(sys.argv) < 2 or len(sys.argv) > 3:
     print >> sys.stderr, "Usage: %s <PLANET> [LARGEST-ADMIN-LEVEL]" % (sys.argv[0],)
     sys.exit(1)
@@ -27,72 +29,184 @@ data_dir = os.path.join(dir, '..', 'data')
 def replace_slashes(s):
     return re.sub(r'/', '_', s)
 
-for admin_level in range(start_admin_level, 12):
+def write_element_to_cached_xml(element):
+    filename = get_cache_filename(*element.name_id_tuple())
+    if os.path.exists(filename):
+        return
+    with open(filename, 'w') as fp:
+        fp.write(etree.tostring(element.to_xml(write_nodes_with_way=True),
+                                pretty_print=True,
+                                encoding="utf-8",
+                                xml_declaration=True))
 
+def wanted_boundary(element):
+    boundary = element.tags.get('boundary', '')
+    if boundary != 'administrative':
+        return None
+    admin_level = element.tags.get('admin_level', '')
+    # if admin_level != '4':
+    if not admin_level:
+        return None
+    if element.get_element_name() == 'node':
+        return None
+    return admin_level
 
+def write_kml(element, admin_level):
+    element_type, element_id = element.name_id_tuple()
+    name = get_name_from_tags(element.tags, element_type, element_id)
+    try:
+        basename = "%s-%s-%s" % (element_type,
+                                 element_id,
+                                 replace_slashes(name))
 
+        level_directory = os.path.join(data_dir, "cache", "planet-al%02d" % (int(admin_level,10),))
+        mkdir_p(level_directory)
+        filename = os.path.join(level_directory, u"%s.kml" % (basename,))
 
-    print "Fetching data at admin level", admin_level
+        if not os.path.exists(filename):
 
-    predicate = '["boundary"="administrative"]'
-    predicate += '["admin_level"="%d"]' % (admin_level,)
-    data = timeout + '(relation%s;way%s;);out body;' % (predicate, predicate)
+            kml, _ = get_kml_for_osm_element_no_fetch(element)
+            if not kml:
+                print "      No data found for %s %s" % (element_type, element_id)
+                return
 
-    print "data is:", data
+            print "      Writing KML to", filename.encode('utf-8')
+            with open(filename, "w") as fp:
+                fp.write(kml)
 
-    file_basename = "admin-level-%02d-worldwide.xml" % (admin_level,)
-    xml_filename = os.path.join(data_dir, "cache", file_basename)
-    overpass_post_request(data, xml_filename)
+    except UnclosedBoundariesException:
+        print "      ... ignoring unclosed boundary"
 
-    level_directory = os.path.join(data_dir, "cache", "al%02d" % (admin_level,))
-    mkdir_p(level_directory)
+def write_kml_if_wanted_element(element):
+    admin_level = wanted_boundary(element)
+    if admin_level:
+        write_kml(element, admin_level)
 
-    def handle_top_level_element(element_type, element_id, tags):
+def deal_with_top_level_element(element, parser):
 
-        if 'admin_level' not in tags:
-            return
-        if tags['admin_level'] != str(admin_level):
-            return
+    global elements_parsed
+    elements_parsed += 1
+    if (elements_parsed % 10000) == 0:
+        print "Time through:", time_through, ", parsed", elements_parsed, "elements"
 
-        name = get_name_from_tags(tags, element_type, element_id)
+    element_tuple = element.name_id_tuple()
+    element_type, element_id = element_tuple
 
-        print "Considering admin boundary:", name.encode('utf-8')
+    debug = element_id == "139312403"
 
-        try:
+    # Nodes are a special case - there are too many of them to deal
+    # with in the same way as ways / relations, since if we created
+    # one file per node we would quickly run out of inodes in the
+    # filesystem.  Instead, we output all nodes in the file with the
+    # way that includes them.
+    if element_type == 'node':
+        if element_id in nodes_needed:
+            nodes_needed[element_id] = element
+        return
 
-            basename = "%s-%s-%s" % (element_type,
-                                     element_id,
-                                     replace_slashes(name))
+    # Now we know we're only dealing with ways or relations.
 
-            filename = os.path.join(level_directory, u"%s.kml" % (basename,))
+    # When admin_level is truthy, this is an element that we
+    # ultimately want to write out as KML:
 
-            if not os.path.exists(filename):
+    admin_level = wanted_boundary(element)
 
-                while True:
-                    
-                    try:
+    # If this way / relation is marked as incomplete from a previous
+    # round, we need to check if that is still the case (i.e. whether
+    # it can now be completely reconstructed).
 
-                        kml, _ = get_kml_for_osm_element(element_type, element_id)
-                        if not kml:
-                            print "      No data found for %s %s" % (element_type, element_id)
-                            return
+    if debug: print "admin_level is:", admin_level
 
-                        print "      Writing KML to", filename.encode('utf-8')
-                        with open(filename, "w") as fp:
-                            fp.write(kml)
+    if element_tuple in incomplete_ways_or_relations:
 
-                    except UnclosedBoundariesException, e:
-                        raise e
-                    except Exception, e:
-                        # Wait before trying again:
-                        time.sleep(wait_after_exceptions)
-                        email_me("error while scraping OSM boundaries",
-                                 unicode(e).encode('utf-8')+"\n"+element_type+"/"+str(element_id),
-                                 'mark@eclipse.ukcod.org.uk',
-                                 'mark-osmscrape@longair.net')
-                    break
+        if debug: print "was in incomplete_ways_or_relations"
 
-        except UnclosedBoundariesException:
-            print "      ... ignoring unclosed boundary"
+        still_missing = element.reconstruct_missing(parser, nodes_needed)
+        if debug: print "still_missing is:", still_missing
+        if still_missing:
+            # It was marked as incomplete, and is still incomplete:
+            if element_type == 'way':
+                # Make sure that every node is in nodes_needed for the
+                # next time through:
+                for node in element:
+                    if debug:  "adding node", node.element_id
+                    new_nodes_needed[node.element_id] = None
+            elif element_type == 'relation':
+                for member, role in element:
+                    if role in OSMXMLParser.IGNORED_ROLES:
+                        continue
+                    if member.element_content_missing:
+                        member_tuple = member.name_id_tuple()
+                        member_type, member_id = member_tuple
+                        if member_type == 'node':
+                            new_nodes_needed[member_id] = None
+                        else:
+                            incomplete_ways_or_relations.add(member_tuple)
+        else: # i.e. it's complete
+            write_element_to_cached_xml(element)
+            incomplete_ways_or_relations.discard(element_tuple)
+            if admin_level:
+                write_kml(element, admin_level)
+    else:
 
-    parse_xml_minimal(xml_filename, handle_top_level_element)
+        if debug: print "wasn't in incomplete_ways_or_relations"
+        # If this is a wanted boundary, it might be one we've never seen before.
+        if admin_level:
+
+            # If there's a version from the filesystem, that will be
+            # more complete:
+            from_cache = parser.get_known_or_fetch(element_type, element_id)
+            if not from_cache.element_content_missing:
+                element = from_cache
+
+            still_missing = element.reconstruct_missing(parser, nodes_needed)
+            if still_missing:
+                incomplete_ways_or_relations.add(element_tuple)
+                for missing_element in still_missing:
+                    missing_type, missing_id = missing_element.name_id_tuple()
+                    if missing_id == "144804249":
+                        print "  got our one (missing element)!"
+                    if missing_type == 'node':
+                        new_nodes_needed[missing_id] = None
+                    else:
+                        incomplete_ways_or_relations.add(missing_element.name_id_tuple())
+            else: # i.e. it's complete
+                write_element_to_cached_xml(element)
+                write_kml(element, admin_level)
+
+nodes_needed = {}
+new_nodes_needed = {}
+incomplete_ways_or_relations = set()
+
+time_through = 1
+
+# import pdb; pdb.set_trace()
+
+while True:
+
+    print "###### Starting parsing..."
+    elements_parsed = 0
+
+    print "At start of run, nodes_needed are:"
+    for k, v in sorted(nodes_needed.items()):
+        print "  ", k, "=>", v
+
+    print "... and incomplete_ways_or_relations are:"
+    for element in sorted(incomplete_ways_or_relations):
+        print "  ", element
+
+    parser = OSMXMLParser(fetch_missing=False, callback=deal_with_top_level_element, cache=False)
+    with open(planet_filename) as fp:
+        xml.sax.parse(fp, parser)
+
+    if not (new_nodes_needed or incomplete_ways_or_relations):
+        "Everything found, exiting."
+        break
+
+    nodes_needed = new_nodes_needed
+    new_nodes_needed = {}
+
+    time_through += 1
+
+    # if time_through > 3:
+    #     break
