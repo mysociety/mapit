da15352ded21e92b9bc7390c229c233554f46b7a
Mark Longair <mhl@pobox.com>
2012-10-04 15:12:21 +0100

    Remove common install script code
    pre-install-as-root is no longer the way that you should
    invoke the install script; instead you should call the
    install-site.sh script in commonlib/bin.

---
+++ bin/site-specific-install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+PARENT_SCRIPT_URL=https://github.com/mysociety/commonlib/blob/master/bin/install-site.sh
+
+misuse() {
+  echo The variable $1 was not defined, and it should be.
+  echo This script should not be run directly - instead, please run:
+  echo   $PARENT_SCRIPT_URL
+  exit 1
+}
+
+# Strictly speaking we don't need to check all of these, but it might
+# catch some errors made when changing install-site.sh
+
+[ -z "$DIRECTORY" ] && misuse DIRECTORY
+[ -z "$UNIX_USER" ] && misuse UNIX_USER
+[ -z "$REPOSITORY" ] && misuse REPOSITORY
+[ -z "$REPOSITORY_URL" ] && misuse REPOSITORY_URL
+[ -z "$BRANCH" ] && misuse BRANCH
+[ -z "$SITE" ] && misuse SITE
+[ -z "$DEFAULT_SERVER" ] && misuse DEFAULT_SERVER
+[ -z "$EC2_HOSTNAME" ] && misuse EC2_HOSTNAME
+[ -z "$HOST" ] && misuse HOST
+[ -z "$DISTRIBUTION" ] && misuse DISTRIBUTION
+[ -z "$VERSION" ] && misuse VERSION
+
+apt-get install -y python-flup gunicorn
+
+install_nginx
+
+install_website_packages
+
+install_postgis
+
+add_postgresql_user
+
+make_log_directory
+
+su -l -c "$REPOSITORY/bin/install-as-user '$UNIX_USER' '$HOST' '$DIRECTORY'" "$UNIX_USER"
+
+install_sysvinit_script
+
+add_website_to_nginx
+
+echo Installation complete - you should now be able to view the site at:
+echo   http://$HOST/
