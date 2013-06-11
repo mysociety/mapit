d31e28f4427c72c5bc05f672fc45b28d09843901
Cesar Martinez Izquierdo <cesar.izq@gmail.com>
2013-06-11 17:46:45 +0200

    Enable the use of remote overpass servers instead of local one, by means of using wget. Parametrize this behaviour on a config file.

---
+++ bin/config.py
@@ -0,0 +1,11 @@
+# whether using local server (osm3s) or remove overpass API
+localserver=True
+# overpass API server
+overpass_server="http://overpass-api.de/api/interpreter"
+# osm3s_db directory
+osm3s_db_directory="/home/overpass/db/"
+# wget path (e.g. "/usr/bin/wget" or "c:/cygwin/bin/wget.exe"). The default value should
+# work if wget command is in the PATH
+wget_path="wget"
+
+
