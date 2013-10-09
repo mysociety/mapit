f09d83716734742de0c4421d5807c95cf1556ac6
Matthew Somerville <matthew@mysociety.org>
2013-10-09 12:49:34 +0100

    Update settings as no longer supporting Django 1.2.

--- project/settings.py
+++ project/settings.py
@@ -61,24 +61,16 @@ if config.get('BUGS_EMAIL'):
     )
     MANAGERS = ADMINS
 
-if django.get_version() >= '1.2':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.contrib.gis.db.backends.postgis',
-            'NAME': config.get('MAPIT_DB_NAME', 'mapit'),
-            'USER': config.get('MAPIT_DB_USER', 'mapit'),
-            'PASSWORD': config.get('MAPIT_DB_PASS', ''),
-            'HOST': config.get('MAPIT_DB_HOST', ''),
-            'PORT': config.get('MAPIT_DB_PORT', ''),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': config.get('MAPIT_DB_NAME', 'mapit'),
+        'USER': config.get('MAPIT_DB_USER', 'mapit'),
+        'PASSWORD': config.get('MAPIT_DB_PASS', ''),
+        'HOST': config.get('MAPIT_DB_HOST', ''),
+        'PORT': config.get('MAPIT_DB_PORT', ''),
     }
-else:
-    DATABASE_ENGINE = 'postgresql_psycopg2'
-    DATABASE_NAME = config.get('MAPIT_DB_NAME', 'mapit')
-    DATABASE_USER = config.get('MAPIT_DB_USER', 'mapit')
-    DATABASE_PASSWORD = config.get('MAPIT_DB_PASS', '')
-    DATABASE_HOST = config.get('MAPIT_DB_HOST', '')
-    DATABASE_PORT = config.get('MAPIT_DB_PORT', '')
+}
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = config.get('DJANGO_SECRET_KEY', '')
@@ -106,32 +98,19 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join( path, '..', 'collected_static' )
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
+# No uploaded media
+MEDIA_ROOT = ''
 MEDIA_URL = ''
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
 # List of callables that know how to import templates from various sources.
-if django.get_version() >= '1.2':
-    TEMPLATE_LOADERS = (
-        'django.template.loaders.filesystem.Loader',
-        # Needs adapting to new class version
-        'mapit.loader.load_template_source',
-    )
-else:
-    TEMPLATE_LOADERS = (
-        'django.template.loaders.filesystem.load_template_source',
-        'mapit.loader.load_template_source',
-    )
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    # Needs adapting to new class version
+    'mapit.loader.load_template_source',
+)
 
 # UpdateCacheMiddleware does ETag setting, and
 # ConditionalGetMiddleware does ETag checking.
@@ -142,11 +121,11 @@ USE_ETAGS = False
 MIDDLEWARE_CLASSES = (
     'mapit.middleware.gzip.GZipMiddleware' if django.get_version() < '1.4' else 'django.middleware.gzip.GZipMiddleware',
     'django.middleware.http.ConditionalGetMiddleware',
-    'mapit.middleware.cache.UpdateCacheMiddleware' if django.get_version() < '1.3' else 'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'mapit.middleware.cache.FetchFromCacheMiddleware' if django.get_version() < '1.3' else 'django.middleware.cache.FetchFromCacheMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
     'mapit.middleware.JSONPMiddleware',
     'mapit.middleware.ViewExceptionMiddleware',
 )
@@ -156,28 +135,14 @@ ROOT_URLCONF = 'urls'
 # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
 # Always use forward slashes, even on Windows.
 # Don't forget to use absolute paths, not relative paths.
-TEMPLATE_DIRS = (
-    os.path.join( package_dir, 'templates', MAPIT_COUNTRY.lower() ),
-    os.path.join( package_dir, 'templates' ),
-)
+TEMPLATE_DIRS = ()
 
-if django.get_version() >= '1.2':
-    TEMPLATE_CONTEXT_PROCESSORS = (
-        'django.core.context_processors.request',
-        'django.contrib.auth.context_processors.auth',
-        'mapit.context_processors.country',
-        'mapit.context_processors.analytics',
-    )
-else:
-    TEMPLATE_CONTEXT_PROCESSORS = (
-        'django.core.context_processors.request',
-        'django.core.context_processors.auth',
-        'mapit.context_processors.country',
-        'mapit.context_processors.analytics',
-        #'django.core.context_processors.debug',
-        #'django.core.context_processors.i18n',
-        #'django.core.context_processors.media',
-    )
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+    'mapit.context_processors.country',
+    'mapit.context_processors.analytics',
+)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -186,14 +151,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.gis',
+    'django.contrib.staticfiles',
 
     'south',
     'mapit',
 )
 
-# use staticfiles if Django recent enough. If not then trust that the webserver
-# can handle serving the content. mySociety currently runs mapit on Django 1.2.x
-# as that is the version in the Debian Squeeze package.
-if django.get_version() >= '1.3':
-    INSTALLED_APPS += ( 'django.contrib.staticfiles', )
-    STATIC_URL = '/static/'
