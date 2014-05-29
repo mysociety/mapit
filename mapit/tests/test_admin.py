e6ede21313b8267bca9cf06a5ac5fad32d866367
Steven <forestorama@gmail.com>
2014-05-29 15:22:16 +0000

    Add smoke test for django admin pages

---
+++ mapit/tests/test_admin.py
@@ -0,0 +1,55 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+
+class AdminViewsTest(TestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_user('superuser', '', 'password')
+        self.superuser.is_staff = True
+        self.superuser.is_superuser = True
+        self.superuser.save()
+        self.client.login(username=self.superuser.username, password='password')
+
+    def tearDown(self):
+        self.client.logout()
+
+    def test_admin_homepage(self):
+        admin_url = reverse("admin:index")
+        resp = self.client.get(admin_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_area_admin_page(self):
+        admin_url = reverse("admin:mapit_area_add")
+        resp = self.client.get(admin_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_type_admin_page(self):
+        admin_url = reverse("admin:mapit_type_add")
+        resp = self.client.get(admin_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_nametype_admin_page(self):
+        admin_url = reverse("admin:mapit_nametype_add")
+        resp = self.client.get(admin_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_codetype_admin_page(self):
+        admin_url = reverse("admin:mapit_codetype_add")
+        resp = self.client.get(admin_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_generation_admin_page(self):
+        admin_url = reverse("admin:mapit_generation_add")
+        resp = self.client.get(admin_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_country_admin_page(self):
+        admin_url = reverse("admin:mapit_country_add")
+        resp = self.client.get(admin_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_postcode_admin_page(self):
+        admin_url = reverse("admin:mapit_postcode_add")
+        resp = self.client.get(admin_url)
+        self.assertEqual(resp.status_code, 200)
