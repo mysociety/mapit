24bad6467cfd79ca189f45b12a986e5acbcc0542
Guglielmo <guglielmo.celata@gmail.com>
2015-02-28 23:22:50 +0100

    geojson areas export task implemented; import postal codes task implemened; generate constituencies csv implemented; import area unions task adjusted

--- mapit/management/commands/mapit_import_area_unions.py
+++ mapit/management/commands/mapit_import_area_unions.py
@@ -6,10 +6,13 @@
 # the GPL.  Based on import_norway_osm.py by Matthew Somerville
 
 import csv
+import logging
 from optparse import make_option
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 from django.core.management.base import LabelCommand
 from django.contrib.gis.geos import GEOSGeometry
+from django.db.models.query import EmptyQuerySet
 from django.utils.six import Iterator
 
 from mapit.models import Area, Generation, Geometry, Country, Type
@@ -17,7 +20,7 @@ from mapit.management.command_utils import save_polygons
 
 
 # CSV format is
-# ID;code;name;area1,area2,...;email;categories
+# ID;code;name;area1,area2,...
 
 # Copied from
 # http://www.mfasold.net/blog/2010/02/python-recipe-read-csvtsv-textfiles-and-ignore-comment-lines/
@@ -40,32 +43,59 @@ class Command(LabelCommand):
     help = 'Import region data'
     args = '<CSV file listing name and which existing areas to combine into regions>'
     option_list = LabelCommand.option_list + (
-        make_option('--commit', action='store_true', dest='commit', help='Actually update the database'),
+        make_option(
+            '--commit', action='store_true', dest='commit', help='Actually update the database'
+        ),
+        make_option(
+            '--country-code', dest='country_code', default='O', help='The country code to assign to created area'
+        ),
+
     )
 
+    FORMAT = "[%(asctime)s] %(levelname)s - %(message)s"
+    logging.basicConfig(format=FORMAT, level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
     def handle_label(self, filename, **options):
+        self.verbosity = options['verbosity']
+
+        # set log level, from verbosity
+        if self.verbosity == '0':
+            self.logger.setLevel(logging.ERROR)
+        elif self.verbosity == '1':
+            self.logger.setLevel(logging.WARNING)
+        elif self.verbosity == '2':
+            self.logger.setLevel(logging.INFO)
+        elif self.verbosity == '3':
+            self.logger.setLevel(logging.DEBUG)
+
         current_generation = Generation.objects.current()
         new_generation = Generation.objects.new()
         if not new_generation:
-            print("Using current generation %d" % current_generation.id)
+            self.logger.info("Using current generation %d" % current_generation.id)
             new_generation = current_generation
         else:
-            print("Using new generation %d" % new_generation.id)
+            self.logger.info("Using new generation %d" % new_generation.id)
 
-        print("Loading file %s" % filename)
+        self.logger.info("Loading file %s" % filename)
         region_line = csv.reader(CommentedFile(open(filename, "rb")),
                                  delimiter=';')
 
-        for regionid, area_type, regionname, area_names, email, categories in region_line:
-            print("Building region '%s'" % regionname)
-            if (-2147483648 > int(regionid) or 2147483647 < int(regionid)):
+        for regionid, area_type, regionname, area_names in region_line:
+            self.logger.info("Building region '%s'" % regionname)
+            if regionid and regionid.trim() != "":
+                regionid = int(regionid)
+            else:
+                regionid = 0
+
+            if (-2147483648 > int(regionid) or 2147483647 < regionid):
                 raise Exception("Region ID %d is outside range of 32-bit integer" % regionid)
 
             if area_names:
                 # Look up areas using the names, find their geometry
                 # and build a geometric union to set as the geometry
                 # of the region.
-                geometry = None
+                geometry = Geometry.objects.none()
                 for name in area_names.split(','):
                     name.strip()
                     name.lstrip()
@@ -73,60 +103,65 @@ class Command(LabelCommand):
                     try:
                         # Use this to allow '123 Name' in area definition
                         areaidnum = int(name.split()[0])
-                        print("Looking up ID '%d'" % areaidnum)
+                        self.logger.debug("Looking up ID '%d'" % areaidnum)
                         args = {
                             'id__exact': areaidnum,
-                            'generation_low__lte': current_generation,
-                            'generation_high__gte': new_generation,
+                            'generation_low__lte': new_generation.id,
+                            'generation_high__gte': current_generation.id,
                             }
                     except (ValueError, IndexError):
-                        print("Looking up name '%s'" % name)
+                        self.logger.debug("Looking up name '%s'" % name)
                         args = {
                             'name__iexact': name,
-                            'generation_low__lte': current_generation,
-                            'generation_high__gte': new_generation,
+                            'generation_low__lte': new_generation.id,
+                            'generation_high__gte': current_generation.id,
                             }
-                    area_id = Area.objects.filter(**args).only('id')
-                    if 1 < len(area_id):
-                        raise Exception("More than one Area named %s, use area ID as well" % name)
+
                     try:
-                        print("ID: %d" % area_id[0].id)
+                        area = Area.objects.get(**args)
+                        self.logger.debug("ID: %d" % area.id)
                         args = {
-                            'area__exact': area_id[0].id,
+                            'area__exact': area.id,
                             }
-                        if geometry:
-                            geometry = geometry | Geometry.objects.filter(**args)
-                        else:
-                            geometry = Geometry.objects.filter(**args)
-                    except:
-                        raise Exception("Area or geometry with name %s was not found!" % name)
-                    unionoutline = geometry.unionagg()
-
-                def update_or_create():
-                    try:
-                        m = Area.objects.get(id=int(regionid))
-                        print("Updating area %s with id %d" % (regionname, int(regionid)))
+                    except Area.MultipleObjectsReturned:
+                        raise Exception("More than one Area named %s, use area ID as well" % name)
                     except Area.DoesNotExist:
-                        print("Creating new area %s with id %d" % (regionname, int(regionid)))
-                        m = Area(
-                            id=int(regionid),
-                            name=regionname,
-                            type=Type.objects.get(code=area_type),
-                            country=Country.objects.get(code='O'),
-                            generation_low=new_generation,
-                            generation_high=new_generation,
-                            )
+                        raise Exception("Area with name %s was not found!" % name)
 
-                    if m.generation_high and current_generation \
-                            and m.generation_high.id < current_generation.id:
-                        raise Exception("Area %s found, but not in current generation %s" % (m, current_generation))
-                    m.generation_high = new_generation
+                    geometry = geometry | Geometry.objects.filter(**args)
 
-                    poly = [GEOSGeometry(unionoutline).ogr]
-                    if options['commit']:
-                        m.save()
-                        save_polygons({regionid: (m, poly)})
 
-                update_or_create()
+                unionoutline = geometry.unionagg()
+
+                try:
+                    m = Area.objects.get(id=regionid)
+                    self.logger.info("Updating area %s with id %d" % (regionname, regionid))
+                except Area.DoesNotExist:
+                    m = Area(
+                        name=regionname,
+                        type=Type.objects.get(code=area_type),
+                        country=Country.objects.get(code=options['country_code']),
+                        generation_low=new_generation,
+                        generation_high=new_generation,
+                        )
+
+
+                    # if id is explicitly specified
+                    # then it is set
+                    if regionid:
+                        m.id = regionid
+
+                    self.logger.info("Creating new area %s with id %d" % (regionname, int(regionid)))
+
+                if m.generation_high and current_generation \
+                        and m.generation_high.id < current_generation.id:
+                    raise Exception("Area %s found, but not in current generation %s" % (m, current_generation))
+                m.generation_high = new_generation
+
+                poly = [GEOSGeometry(unionoutline).ogr]
+                if options['commit']:
+                    m.save()
+                    save_polygons({regionid: (m, poly)})
+
             else:
                 raise Exception("No area names found for region with name %s!" % regionname)
---
+++ mapit_it/management/commands/mapit_IT_import_postal_codes.py
@@ -0,0 +1,185 @@
+# This is a generic script for importing postal codes in some format from a CSV
+# file. The CSV file should have the following columns:
+#   Postal code, Latitude, Longitude
+# By default in those positions, though you can specify other column numbers on
+# the command line
+
+import csv
+from optparse import make_option
+from django.contrib.gis.geos import Point
+from django.core.management.base import LabelCommand
+from django.conf import settings
+import requests
+import time
+from mapit.models import Postcode
+
+
+class Command(LabelCommand):
+    help = 'Import Postal codes from a CSV file or files'
+    args = '<CSV files>'
+    count = {'total': 0, 'updated': 0, 'unchanged': 0, 'created': 0}
+    often = 10
+    nominatim_url = "http://nominatim.openstreetmap.org/"
+
+    option_defaults = {}
+    option_list = LabelCommand.option_list + (
+        make_option(
+            '--code-field',
+            action='store',
+            dest='code-field',
+            default=1,
+            help='The column of the CSV containing the postal code (default 1, first)'
+        ),
+        make_option(
+            '--coord-field-lat',
+            action='store',
+            dest='coord-field-lat',
+            default=None,
+            help='The column of the CSV containing the lat/y co-ordinate (default 2)'
+        ),
+        make_option(
+            '--coord-field-lon',
+            action='store',
+            dest='coord-field-lon',
+            default=None,
+            help='The column of the CSV containing the lon/x co-ordinate (default --coord-field-lat + 1)'
+        ),
+        make_option(
+            '--header-row',
+            action='store_true',
+            dest='header-row',
+            default=False,
+            help='Set if the CSV file has a header row'
+        ),
+        make_option(
+            '--no-location',
+            action="store_false",
+            dest='location',
+            default=True,
+            help='Set if the postal codes have no associated location (still useful for existence checks)'
+        ),
+        make_option(
+            '--srid',
+            action="store",
+            dest='srid',
+            default=4326,
+            help='The SRID of the projection for the data given (default 4326 WGS-84)'
+        ),
+        make_option(
+            '--strip',
+            action="store_true",
+            dest='strip',
+            default=False,
+            help='Whether to strip all spaces from the postal code before import'
+        ),
+        make_option(
+            '--tabs',
+            action="store_true",
+            dest='tabs',
+            default=False,
+            help='If the CSV file actually uses tab as its separator'
+        ),
+        make_option(
+            '--nominatim-sleep-time',
+            action="store",
+            dest='nominatim-sleep-time',
+            default="0.25",
+            help='The n. of seconds to wait before two subsequent requests'
+        ),
+    )
+
+    def handle_label(self, file, **options):
+        self.process(file, options)
+
+    def process(self, file, options):
+        options.update(self.option_defaults)
+        if options['tabs']:
+            reader = csv.reader(open(file), dialect='excel-tab')
+        else:
+            reader = csv.reader(open(file))
+        if options['header-row']:
+            next(reader)
+        for row in reader:
+            self.code = row[int(options['code-field']) - 1].strip()
+            if options['strip']:
+                self.code = self.code.replace(' ', '')
+            if not self.pre_row(row, options):
+                continue
+            pc = self.handle_row(row, options)
+            self.post_row(pc)
+        self.print_stats()
+
+    def pre_row(self, row, options):
+        return True
+
+    def post_row(self, pc):
+        return True
+
+    def handle_row(self, row, options):
+        if not options['location']:
+            return self.do_postcode()
+
+        if not options['coord-field-lat']:
+            # request lat and lon to nominatim
+            req = "{0}search?format=json&country=it&postalcode={1}".format(
+                self.nominatim_url, self.code
+            )
+            try:
+                r = requests.get(req).json()[0]
+                lat = float(r['lat'])
+                lon = float(r['lon'])
+            except Exception as e:
+                print u"Error while requesting {0}".format(req)
+                print u"Exception: {0}".format(e)
+                print u"Skipping."
+                return
+            # sleep before successive request
+            time.sleep(float(options['nominatim-sleep-time']))
+        else:
+            if not options['coord-field-lon']:
+                options['coord-field-lon'] = int(options['coord-field-lat']) + 1
+            lat = float(row[int(options['coord-field-lat']) - 1])
+            lon = float(row[int(options['coord-field-lon']) - 1])
+        srid = int(options['srid'])
+        location = Point(lon, lat, srid=srid)
+        return self.do_postcode(location, srid)
+
+    # Want to compare co-ordinates so can't use straightforward
+    # update_or_create
+    def do_postcode(self, location=None, srid=None):
+        try:
+            print u"code: {0}".format(self.code)
+            pc = Postcode.objects.get(postcode=self.code)
+            if location:
+                curr_location = (pc.location[0], pc.location[1])
+                if settings.MAPIT_COUNTRY == 'GB':
+                    if pc.postcode[0:2] == 'BT':
+                        curr_location = pc.as_irish_grid()
+                    else:
+                        pc.location.transform(27700)  # Postcode locations are stored as WGS84
+                        curr_location = (pc.location[0], pc.location[1])
+                    curr_location = tuple(map(round, curr_location))
+                elif srid != 4326:
+                    pc.location.transform(srid)  # Postcode locations are stored as WGS84
+                    curr_location = (pc.location[0], pc.location[1])
+                if curr_location[0] != location[0] or curr_location[1] != location[1]:
+                    pc.location = location
+                    pc.save()
+                    self.count['updated'] += 1
+                else:
+                    self.count['unchanged'] += 1
+            else:
+                self.count['unchanged'] += 1
+        except Postcode.DoesNotExist:
+            pc = Postcode.objects.create(postcode=self.code, location=location)
+            self.count['created'] += 1
+        self.count['total'] += 1
+        if self.count['total'] % self.often == 0:
+            self.print_stats()
+        return pc
+
+    def print_stats(self):
+        print("Imported %d (%d new, %d changed, %d same)" % (
+            self.count['total'], self.count['created'],
+            self.count['updated'], self.count['unchanged']
+        ))
