b23bccdbcc51d34c0bd4f6319f53dcdd23872e38
Jon Kristensen <info@jonkri.com>
2015-05-09 23:03:29 +0200

    Add Sweden to list of MapIt countries

---
+++ mapit_se/countries.py
@@ -0,0 +1,19 @@
+import re
+
+# SRID to also output area geometry information in
+#area_geometry_srid = 32633 # Norway's used srid
+area_geometry_srid = 30800 # found here:
+# http://postgis.refractions.net/pipermail/postgis-users/2003-March/002207.html
+
+# Swedish postcodes are seven digits. Some put "se-" in front, but
+# this is ignored here.
+def is_valid_postcode(pc):
+#    if re.match('\d{7}$', pc):
+        return True
+#    return False
+
+# Should match one, two and three digits.
+def is_valid_partial_postcode(pc):
+    if re.match('\d{1,3}$', pc):
+        return True
+    return False
