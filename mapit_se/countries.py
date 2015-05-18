9050ddfe0c170ebd40646328ebae3ff9039f5182
Jon Kristensen <jon.kristensen@lambdatrade.com>
2015-05-18 19:00:52 +0200

    Sweden: Update is_valid_postcode regular expression

--- mapit_se/countries.py
+++ mapit_se/countries.py
@@ -5,12 +5,12 @@ import re
 area_geometry_srid = 30800 # found here:
 # http://postgis.refractions.net/pipermail/postgis-users/2003-March/002207.html
 
-# Swedish postcodes are seven digits. Some put "se-" in front, but
-# this is ignored here.
+# Swedish postcodes are five digits, with a possible space before the last two.
+# Some put "se-" in front, but this is ignored here.
 def is_valid_postcode(pc):
-#    if re.match('\d{7}$', pc):
+    if re.match('^\d{3}\s?\d{2}$', pc):
         return True
-#    return False
+    return False
 
 # Should match one, two and three digits.
 def is_valid_partial_postcode(pc):
