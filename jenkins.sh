f4fa07a40fd53d673dd74987b6e12c2757862a05
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2015-10-06 11:48:38 +0100

    Add Jenkins scripts for master and branch builds on CI

---
+++ jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+rm -rf ./test-venv
+
+virtualenv --no-site-packages ./test-venv
+echo 'Installing packages...'
+./test-venv/bin/pip -q install --download-cache "${HOME}/bundles/${JOB_NAME}" -r requirements.txt
+
+echo 'Running tests...'
+./test-venv/bin/python manage.py test
+
+echo 'Done'
+rm -rf ./test-venv
+exit 0
