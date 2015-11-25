eef599c596a0ff3d105e20a9b5d2f7178dfa1fa6
Murray Steele <muz@h-lame.com>
2015-11-25 13:32:12 +0000

    Handle NI postcodes in ONSPD importer
    We add an option to allow importing the NI postcodes at the same time as the rest of the postcodes.  The option takes 3 values: 'include', 'exclude', 'only' with the same behaviour as the --crown-dependencies option:
    
    * 'include' will import NI postcodes
    * 'exclude' will not import NI postcodes
    * 'only' will only import NI postcodes
    
    The default is 'exclude' to maintain previous behaviour.  Unlike Crown Dependency postcodes NI postcodes might have location data, and so the --allow-no-location-postcodes setting (default false) does affect how we import NI postcodes.
    
    Setting both --crown-dependencies and --northern-ireland to 'only' is an error and will halt the importer before it begins.
    
    We've also updated the documentation provided by the options to be clearer about how the various options interact.
    
    Unlike the old nspd_ni importer that relied on the nspd_ni_areas importer to be run and the ni-electoral-areas.csv to directly assign areas to NI postcodes, this importer has no special handling.  We assume that the new OSNI importer has been run and the relevant shapefiles have been imported, much like we assume that the boundary-line importer has been run to provide the areas for the rest of the UK.

--- mapit_gb/management/commands/mapit_UK_import_onspd.py
+++ mapit_gb/management/commands/mapit_UK_import_onspd.py
@@ -89,7 +89,10 @@ from mapit.management.commands.mapit_import_postal_codes import Command
 
 
 class Command(Command):
-    help = 'Imports UK postcodes from the NSPD, excluding NI and Crown Dependencies'
+    help = ('Imports UK postcodes from the ONSPD.\n\nBy default imports only '
+            'live GB postcodes with a lat/lng, options are available to also '
+            'import terminated, NI, or Crown Dependency postcodes and those '
+            'without locations.')
     args = '<ONSPD CSV file>'
     option_defaults = {'header-row': True, 'strip': True, 'srid': 27700, 'coord-field-lon': 10, 'coord-field-lat': 11}
     option_list = Command.option_list + (
@@ -98,14 +101,19 @@ class Command(Command):
             action='store_true',
             dest='include-terminated',
             default=False,
-            help='Set if you want to import terminated postcodes'
+            help=('Set if you want to import terminated postcodes.  Affects all '
+                  'postcodes: GB, NI, and Crown Dependencies')
         ),
         make_option(
             '--allow-no-location-postcodes',
             action='store_true',
             dest='include-no-location',
             default=False,
-            help='Set if you want to import postcodes without location info (quality: 9).  Note that Crown Dependency postcodes have no location and if you choose to import these postcodes they will be imported regardless of your choice for this option.'
+            help=('Set if you want to import postcodes without location info '
+                  '(quality: 9).  Affects GB and NI postcodes only.  Crown '
+                  'Dependency postcodes have no location and will be imported '
+                  'based on the value of --crown-dependencies, regardless of '
+                  'your choice for this option.')
         ),
         make_option(
             '--crown-dependencies',
@@ -119,33 +127,56 @@ class Command(Command):
                   'imported solely based on this option, regardless of the '
                   'presence of --allow-no-location-postcodes.')
         ),
+        make_option(
+            '--northern-ireland',
+            action='store',
+            dest='northern-ireland',
+            default='exclude',
+            help=('How to handle Northern Ireland postocdes.  Set to "include" '
+                  'to import them, set to "exclude" to ignore them, set to '
+                  '"only" to import only these. (Default: exclude).  You should'
+                  ' run mapit_UK_import_onspd_ni_areas before trying to import '
+                  'any NI postcodes.')
+        ),
     )
 
     def handle_label(self, file, **options):
-        # Check our crown-dependencies option is correct
-        if not options['crown-dependencies'] in ('include', 'exclude', 'only'):
-            raise RuntimeError('Invalid value for --crown-dependencies "%s" must be "include", "exclude", or "only".  ' % options['crown-dependencies'])
+        self.check_options_are_valid(options)
         self.process(file, options)
 
     def pre_row(self, row, options):
-        if self.northern_ireland(row):
-            return False  # NI handled by other importer
-        elif self.reject_row_based_on_termination_data(row, options):
+        if self.reject_row_based_on_termination_data(row, options):
             return False  # Terminated postcode
         elif self.reject_row_based_on_location_data(row, options):
             return False  # go no further unless we want codes with no location
         elif self.reject_row_based_on_crown_dependency_data(row, options):
             return False  # handle crown depenency options
-        else:
-            return True
+        elif self.reject_row_based_on_northern_ireland_data(row, options):
+            return False  # handle northern ireland options
+
+        return True
+
+    def check_options_are_valid(self, options):
+        # Check our crown-dependencies option is valid
+        if not options['crown-dependencies'] in ('include', 'exclude', 'only'):
+            raise RuntimeError(('Invalid value for --crown-dependencies "%s" must'
+                                ' be "include", "exclude", or "only".') % options['crown-dependencies'])
+        # Check our northern-ireland option is valid
+        if not options['northern-ireland'] in ('include', 'exclude', 'only'):
+            raise RuntimeError(('Invalid value for --northern-ireland "%s" must '
+                                'be "include", "exclude", or "only".') % options['northern-ireland'])
+        # Check we're not trying to "only" import both
+        if (options['crown-dependencies'] == 'only') and (options['northern-ireland'] == 'only'):
+            raise RuntimeError(('Cannot support "only" as value for both '
+                                '--northern-ireland and --crown-dependencies'))
 
     def location_available_for_row(self, row):
         return row[11] != '9'  # PO Box etc.
 
-    def crown_dependency(self, _row):
+    def crown_dependency_postcode(self):
         return self.code[0:2] in ('GY', 'JE', 'IM')
 
-    def northern_ireland(self, _row):
+    def northern_ireland_postcode(self):
         return self.code[0:2] == 'BT'
 
     def reject_row_based_on_location_data(self, row, options):
@@ -163,12 +194,20 @@ class Command(Command):
         # crown dependencies have no location data in ONSPD so we allow the
         # row and defer the decision to reject the row until we examine the
         # 'crown-dependencies' option.
-        return options['include-no-location'] or self.crown_dependency(row)
+        return options['include-no-location'] or self.crown_dependency_postcode()
 
     def reject_row_based_on_crown_dependency_data(self, row, options):
         if self.crown_dependency_postcode():
             return options['crown-dependencies'] == 'exclude'  # reject if we should exclude these codes
         elif options['crown-dependencies'] == 'only':
+            return True  # if we're only importing these codes, reject other codes
+        else:
+            return False  # otherwise keep
+
+    def reject_row_based_on_northern_ireland_data(self, row, options):
+        if self.northern_ireland_postcode():
+            return options['northern-ireland'] == 'exclude'  # reject if we should exclude these codes
+        elif options['northern-ireland'] == 'only':
             return True  # if we're only importing these codes, reject other codes
         else:
             return False  # otherwise keep
