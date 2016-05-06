mysociety/mapit

commit d692d2788167516a9963563937bb8ed8a0e475e1

Use reverse so more redirects work when MapIt is mounted on a subpath
If you've included MapIt in another Django project, e.g. with:

    urlpatterns += patterns('',
        (r'^mapit/', include('mapit.urls')),
    )

... then some of the views that returned a redirect to a URL
constructed with string formatting would redirect to a path
that didn't include the 'mapit/' prefix.  If these paths are
constructed with reverse() instead, it works properly.

This also introduces some new URL pattern names, so the URL
patterns can be referred to more concisely.

mapit/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^postcode/partial/(?P<postcode>[A-Za-z0-9 ]+)%s$' % format_end,
         postcodes.partial_postcode, name="mapit-postcode-partial"),
 
-    url(r'^area/(?P<area_id>[0-9A-Z]+)%s$' % format_end, areas.area),
+    url(r'^area/(?P<area_id>[0-9A-Z]+)%s$' % format_end, areas.area, name='area'),
     url(r'^area/(?P<area_id>[0-9]+)/example_postcode%s$' % format_end, postcodes.example_postcode_for_area),
     url(r'^area/(?P<area_id>[0-9]+)/children%s$' % format_end, areas.area_children),
     url(r'^area/(?P<area_id>[0-9]+)/geometry$', areas.area_geometry),
@@ -38,11 +38,11 @@ urlpatterns = [
 
     url(r'^point/$', areas.point_form_submitted),
     url(r'^point/(?P<srid>[0-9]+)/(?P<x>%s),(?P<y>%s)(?:/(?P<bb>box))?%s$' % (number, number, format_end),
-        areas.areas_by_point),
+        areas.areas_by_point, name='areas-by-point'),
     url(r'^point/latlon/(?P<lat>%s),(?P<lon>%s)(?:/(?P<bb>box))?%s$' % (number, number, format_end),
-        areas.areas_by_point_latlon),
+        areas.areas_by_point_latlon, name='areas-by-point-latlon'),
     url(r'^point/osgb/(?P<e>%s),(?P<n>%s)(?:/(?P<bb>box))?%s$' % (number, number, format_end),
-        areas.areas_by_point_osgb),
+        areas.areas_by_point_osgb, name='areas-by-point-osgb'),
 
     url(r'^nearest/(?P<srid>[0-9]+)/(?P<x>%s),(?P<y>%s)%s$' % (number, number, format_end), postcodes.nearest),
 

mapit_gb/countries.py
@@ -1,5 +1,6 @@
 import re
 
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
 from mapit.shortcuts import get_object_or_404
@@ -24,12 +25,15 @@ def area_code_lookup(request, area_id, format):
         args['type__code'] = 'OMF'
 
     area = get_object_or_404(Area, **args)
-    path = '/area/%d%s' % (area.id, '.%s' % format if format else '')
+    area_kwargs = {'area_id': area.id}
+    if format:
+        area_kwargs['format'] = format
+    redirect_path = reverse('area', kwargs=area_kwargs)
     # If there was a query string, make sure it's passed on in the
     # redirect:
     if request.META['QUERY_STRING']:
-        path += "?" + request.META['QUERY_STRING']
-    return HttpResponseRedirect(path)
+        redirect_path += "?" + request.META['QUERY_STRING']
+    return HttpResponseRedirect(redirect_path)
 
 
 def canonical_postcode(pc):

mapit/views/areas.py
@@ -341,7 +341,10 @@ def area_from_code(request, code_type, code_value, format='json'):
     except Area.MultipleObjectsReturned:
         message = _('There were multiple areas that matched code {0} = {1}.').format(code_type, code_value)
         raise ViewException(format, message, 500)
-    return HttpResponseRedirect("/area/%d%s" % (area.id, '.%s' % format if format else ''))
+    area_kwargs = {'area_id': area.id}
+    if format:
+        area_kwargs['format'] = format
+    return HttpResponseRedirect(reverse('area', kwargs=area_kwargs))
 
 
 @ratelimit(minutes=3, requests=100)
@@ -388,28 +391,36 @@ def areas_by_point(request, srid, x, y, bb=False, format='json'):
 
 @ratelimit(minutes=3, requests=100)
 def areas_by_point_latlon(request, lat, lon, bb=False, format=''):
-    return HttpResponseRedirect("/point/4326/%s,%s%s%s" % (
-        lon, lat, "/box" if bb else '', '.%s' % format if format else '')
-    )
+    point_kwargs = {'srid': '4326', 'x': lon, 'y': lat}
+    if bb:
+        point_kwargs['bb'] = bb
+    if format:
+        point_kwargs['format'] = format
+    redirect_path = reverse('areas-by-point', kwargs=point_kwargs)
+    return HttpResponseRedirect(redirect_path)
 
 
 @ratelimit(minutes=3, requests=100)
 def areas_by_point_osgb(request, e, n, bb=False, format=''):
-    return HttpResponseRedirect("/point/27700/%s,%s%s%s" % (
-        e, n, "/box" if bb else '', '.%s' % format if format else '')
-    )
+    point_kwargs = {'e': e, 'n': n}
+    if bb:
+        point_kwargs['bb'] = bb
+    if format:
+        point_kwargs['format'] = format
+    redirect_path = reverse('areas-by-point-osgb', kwargs=point_kwargs)
+    return HttpResponseRedirect(redirect_path)
 
 
 def point_form_submitted(request):
     latlon = request.POST.get('pc', None)
     if not request.method == 'POST' or not latlon:
-        return redirect('/')
+        return redirect('mapit_index')
     m = re.match('\s*([0-9.-]+)\s*,\s*([0-9.-]+)', latlon)
     if not m:
-        return redirect('/')
+        return redirect('mapit_index')
     lat, lon = m.groups()
     return redirect(
-        'mapit.views.areas.areas_by_point',
+        'areas-by-point',
         srid=4326, x=lon, y=lat, format='html'
     )
 

commit 394890d64f2afcda072e3b4b64c881ec44f92e14

Fix reversing of ONS coded polygon URLs.
d692d27 broke the reversing of polygon (kml/geojson/wkt) area
URLs when given with an ONS or GSS code.

mapit/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     url(r'^area/(?P<area_id>[0-9]+)/covered%s$' % format_end, areas.area_covered),
     url(r'^area/(?P<area_id>[0-9]+)/coverlaps%s$' % format_end, areas.area_coverlaps),
     url(r'^area/(?P<area_id>[0-9]+)/intersects%s$' % format_end, areas.area_intersects),
-    url(r'^area/(?P<area_id>[0-9A-Z]+)\.(?P<format>kml|geojson|wkt)$', areas.area_polygon),
+    url(r'^area/(?P<area_id>[0-9A-Z]+)\.(?P<format>kml|geojson|wkt)$', areas.area_polygon, name='area_polygon'),
     url(r'^area/(?P<srid>[0-9]+)/(?P<area_id>[0-9]+)\.(?P<format>kml|json|geojson|wkt)$',
         areas.area_polygon),
 

mapit_gb/countries.py
@@ -1,6 +1,6 @@
 import re
 
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, NoReverseMatch
 from django.http import HttpResponseRedirect
 
 from mapit.shortcuts import get_object_or_404
@@ -28,7 +28,11 @@ def area_code_lookup(request, area_id, format):
     area_kwargs = {'area_id': area.id}
     if format:
         area_kwargs['format'] = format
-    redirect_path = reverse('area', kwargs=area_kwargs)
+    # We're called either by area or area_polygon
+    try:
+        redirect_path = reverse('area', kwargs=area_kwargs)
+    except NoReverseMatch:
+        redirect_path = reverse('area_polygon', kwargs=area_kwargs)
     # If there was a query string, make sure it's passed on in the
     # redirect:
     if request.META['QUERY_STRING']:

mapit_gb/tests.py
@@ -57,6 +57,16 @@ class GBViewsTest(TestCase):
             area=self.area, polygon=polygon,
         )
 
+        code_type_gss = models.CodeType.objects.create(
+            code='gss',
+            description='GSS codes from ONS',
+        )
+        models.Code.objects.create(
+            area=self.area,
+            type=code_type_gss,
+            code='E05000025',
+        )
+
     def test_postcode_json(self):
         pc = self.postcode.postcode
         url = '/postcode/%s' % urllib.parse.quote(pc)
@@ -85,6 +95,7 @@ class GBViewsTest(TestCase):
                     'country_name': '-',
                     'codes': {
                         'CodeType': 'CODE',
+                        'gss': 'E05000025',
                     },
                     'all_names': {},
                 }
@@ -140,3 +151,10 @@ class GBViewsTest(TestCase):
         response = self.client.get(url)
         pc = self.postcode.postcode
         self.assertContains(response, '"/postcode/%s"' % url_postcode(pc))
+
+    def test_gss_code(self):
+        response = self.client.get('/area/E05000025')
+        self.assertRedirects(response, '/area/%d' % self.area.id)
+
+        response = self.client.get('/area/E05000025.geojson')
+        self.assertRedirects(response, '/area/%d.geojson' % self.area.id)