e7a6d428c6bf160f560a9a1cd7c7ae409a01b288
David Basalla <davidbasalla@gmail.com>
2016-05-24 13:49:56 +0100

    Update missing codes command to accept arguments
    As part of the work to import local authority slugs, we need to be able to tell which Areas (that are Local Authorities) do not have a slug. We can reuse the existing 'mapit_UK_show_missing_codes' command by allowing it to accept arguments such as the `code_types` and `area_types`. We set the default values to be the previously hardcoded values so the script can still be called as it was before. However we do not want to check area_type 'EUR' for code_type 'govuk_slug', as we do not assign slugs to that area type. We add this exception to the code as a method call `is_code_type_required_for_area_type`.

--- mapit_gb/management/commands/mapit_UK_show_missing_codes.py
+++ mapit_gb/management/commands/mapit_UK_show_missing_codes.py
@@ -1,9 +1,24 @@
 from django.core.management.base import NoArgsCommand
 from mapit.models import Area, Generation
+from optparse import make_option
 
 
 class Command(NoArgsCommand):
     help = "Prints results of queries to check we aren't missing SNAC and GSS codes"
+    option_list = NoArgsCommand.option_list + (
+        make_option(
+            "--code_types", dest="code_types",
+            help="The list of code types to search for",
+            default="ons,gss,govuk_slug"),
+        make_option(
+            "--area_types", dest="area_types",
+            help="The list of area types to search for",
+            # These types are from
+            # http://github.com/alphagov/imminence/blob/26f6c9e5969a9e09bd24d6e2e4ebfe55dba1d997/config/routes.rb#L13
+            # except for COI which is used in Frontend:
+            # https://github.com/alphagov/frontend/blob/aed183cf3ed6a1e77cf3ec11f7dd6c238a7557cf/lib/location_identifier.rb#L4
+            default="EUR,CTY,DIS,LBO,LGD,MTD,UTA,COI"),
+        )
 
     def handle_noargs(self, **options):
         current_generation = Generation.objects.current()
@@ -16,12 +31,13 @@ class Command(NoArgsCommand):
             gen_id=current_generation.id
         )
 
-        # These types are from
-        # http://github.com/alphagov/imminence/blob/26f6c9e5969a9e09bd24d6e2e4ebfe55dba1d997/config/routes.rb#L13
-        # except for COI which is used in Frontend:
-        # https://github.com/alphagov/frontend/blob/aed183cf3ed6a1e77cf3ec11f7dd6c238a7557cf/lib/location_identifier.rb#L4
-        used_area_types = ('EUR', 'CTY', 'DIS', 'LBO', 'LGD', 'MTD', 'UTA', 'COI')
-        used_code_types = ('ons', 'gss')  # 'ons' is the SNAC code
+        used_area_types = options['area_types'].split(',')
+        used_code_types = options['code_types'].split(',')
+
+        print 'Checking {area_types} for missing {code_types}'.format(
+            area_types=used_area_types,
+            code_types=used_code_types
+        )
 
         for area_type in used_area_types:
             areas = current_areas.filter(type__code=area_type)
@@ -30,6 +46,8 @@ class Command(NoArgsCommand):
                 type=area_type
             )
             for code_type in used_code_types:
+                if self.is_code_type_not_required_for_area_type(code_type, area_type): continue
+
                 areas_without_codes = areas.exclude(codes__type__code=code_type)
                 if areas_without_codes.count() > 0:
                     print '  {count} {type} areas have no {code_type} code:'.format(
@@ -45,3 +63,6 @@ class Command(NoArgsCommand):
                             gen_high=area.generation_high_id,
                             country=area.country
                         )
+
+    def is_code_type_not_required_for_area_type(self, code_type, area_type):
+        return (code_type == "govuk_slug" and area_type == "EUR")
