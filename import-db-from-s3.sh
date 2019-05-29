bd60b4597848081585a5486320f8ba7c78b4629c
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-05-29 14:22:29 +0100

    Update database dump URL to latest version
    I've just uploaded this version to S3 following on from the latest
    import after boundary changes.

--- import-db-from-s3.sh
+++ import-db-from-s3.sh
@@ -2,8 +2,8 @@
 set -e
 
 # get the data from s3
-sudo -u deploy curl 'https://s3-eu-west-1.amazonaws.com/govuk-custom-formats-mapit-storage-production/source-data/2018-12/mapit-Dec2018.sql.gz' -o mapit.sql.gz
-if ! echo "3114fa44c0c93ea3d8ed077c0bd8e8ddebaa462c mapit-Dec2018.sql.gz" | sha1sum -c -; then
+sudo -u deploy curl 'https://s3-eu-west-1.amazonaws.com/govuk-custom-formats-mapit-storage-production/source-data/2019-05/mapit.sql.gz' -o mapit.sql.gz
+if ! echo "f676f7219659858cf44f7318d2ad4486f6059bd9 mapit.sql.gz" | sha1sum -c -; then
   echo "SHA1 does not match downloaded file!"
   exit 1
 fi
