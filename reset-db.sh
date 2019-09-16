bd920fb111d11647970aa8b9777d5d9b6e1b40ea
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-09-16 10:38:24 +0100

    Exit on the first error in the reset-db.sh script
    So if dropping the database fails, the script stops there, rather than
    going on to try and create the database.

--- reset-db.sh
+++ reset-db.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 sudo -u postgres dropdb mapit
 sudo -u postgres createdb --owner mapit mapit
 sudo -u postgres psql -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;" mapit
