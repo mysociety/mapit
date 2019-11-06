55a506b5f4ad89670831c95dfbdd5d6e1f62ab83
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-11-06 13:35:50 +0000

    Run the app using Python 3
    We're going to start deploying Mapit using Python 3 so we should run the
    app locally using Python 3. This ensures that we're running the app in the
    same environment as production.

--- startup.sh
+++ startup.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -e
 
 DIRECTORY='.venv'
 if [ ! -d "$DIRECTORY" ]; then
-  virtualenv --no-site-packages "$DIRECTORY"
+  virtualenv -p python3 --no-site-packages "$DIRECTORY"
 fi
 
-$DIRECTORY/bin/pip install --upgrade pip wheel setuptools
-$DIRECTORY/bin/pip install -r requirements.txt
-$DIRECTORY/bin/python ./manage.py migrate -v 0
-$DIRECTORY/bin/python ./manage.py runserver 3108
+$DIRECTORY/bin/python -m pip install --upgrade pip wheel setuptools
+$DIRECTORY/bin/python -m pip install -r requirements.txt
+$DIRECTORY/bin/python manage.py migrate -v 0
+$DIRECTORY/bin/python manage.py runserver 3108
