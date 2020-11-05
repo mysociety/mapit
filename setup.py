3fd6a26b0ff445a364fccfe133702422d279c8cf
M Somerville <matthew-github@dracos.co.uk>
2020-11-05 16:45:04 +0000

    Version 3.0.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def read_file(filename):
 
 setup(
     name='django-mapit',
-    version='2.0',
+    version='3.0',
     description=(
         'A web service for mapping postcodes and points to current or past '
         'administrative area information and polygons.'),
