51365df3e6cd1d867d206d66dd3a1fed03a00cfd
M Somerville <matthew-github@dracos.co.uk>
2021-06-16 17:52:55 +0100

    Cope better if within is not provided in settings.

--- mapit/context_processors.py
+++ mapit/context_processors.py
@@ -5,7 +5,7 @@ def country(request):
     return {
         'country': settings.MAPIT_COUNTRY,
         'area_srid_units': 'degrees' if settings.MAPIT_AREA_SRID == 4326 else 'metres',
-        'within_maximum': settings.MAPIT_WITHIN_MAXIMUM,
+        'within_maximum': getattr(settings, 'MAPIT_WITHIN_MAXIMUM', 0),
         'postcodes_available': settings.POSTCODES_AVAILABLE,
         'partial_postcodes_available': settings.PARTIAL_POSTCODES_AVAILABLE,
     }
