691b14b38dc5e47bc89f2449b96c73089b807d2f
Matthew Somerville <matthew@mysociety.org>
2023-11-07 10:14:08 +0000

    [UK] October 2023 Boundary-Line import.
    Control file and one off script to deal with issues
    caused by errors in the previous release.

---
+++ mapit_gb/controls/2023-10.py
@@ -0,0 +1,30 @@
+# A control file for importing October 2023 Boundary-Line.
+
+# This control file assumes previous Boundary-Lines have been imported,
+# because it uses that information. If this is a first import, use the
+# first-gss control file.
+
+from mapit.models import Area, Generation
+
+
+def code_version():
+    return 'gss'
+
+
+def check(name, type, country, geometry, ons_code, **args):
+    """Should return True if this area is NEW, False if we should match against
+    an ONS code, or an Area to be used as an override instead."""
+
+    missing_parishes = {
+        'Hargrave CP': 'E04009317',
+    }
+    if type == 'CPC' and name in missing_parishes.keys():
+        # In May 2023 Hargrave CP was given the GSS ID that should have been
+        # given to Ousden. So it is not in the current generation and isn't
+        # found, and we have to help it along
+        a = Area.objects.get(codes__type__code='gss', codes__code=missing_parishes[name])
+        a.generation_high = Generation.objects.current()
+        return a
+
+    # This is the default
+    return False
